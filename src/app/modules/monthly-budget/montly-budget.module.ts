0b16bde0dcafc301d3023f46592563f2f094b3b1
MariaNse <19masha96@ukr.net>
2021-01-09 23:32:21 +0200

    tiny fixes

--- src/app/modules/monthly-budget/montly-budget.module.ts
+++ src/app/modules/monthly-budget/montly-budget.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { MonthlyBudgetEmptyStateComponent } from './monthly-budget-empty-state/monthly-budget-empty-state.component'
 import { MonthlyBudgetRoutingModule } from './montly-budget-routing.module'
+import { MatButtonModule } from '@angular/material'
 
 @NgModule({
   declarations: [MonthlyBudgetEmptyStateComponent],
-  imports: [CommonModule, MonthlyBudgetRoutingModule],
+  imports: [CommonModule, MonthlyBudgetRoutingModule, MatButtonModule],
   entryComponents: [],
   exports: [MonthlyBudgetEmptyStateComponent],
 })
